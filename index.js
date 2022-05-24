add861ff34604d6667c2f99c323f2e79a8e66af7
mzltest <52124882+mzltest@users.noreply.github.com>
2022-05-24 19:54:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-let arc = require('@architect/functions')
-let parseBody = arc.http.helpers.bodyParser
-
 exports.handler = async function http (request) {
-  console.log(request.body)     // 'Z3JlZXRpbmc9aG93ZHk='
-  let body = parseBody(request) // Pass the entire request object
-  let greeting = body.greeting  // 'howdy'
-  return {
-    statusCode: 200,
-    headers: { 'content-type': 'text/html; charset=utf8' },
-    body: greeting
-  }
+  let response = { ok: true }
+  return response
 }
